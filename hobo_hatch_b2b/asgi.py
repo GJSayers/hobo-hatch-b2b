57207cf4619da16861c801c05ba246e296b00d41
GJSayers <gemma.sayers1@btinternet.com>
2021-10-05 15:59:23 +0000

    initial commit

---
+++ hobo_hatch_b2b/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hobo_hatch_b2b project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hobo_hatch_b2b.settings')
+
+application = get_asgi_application()
