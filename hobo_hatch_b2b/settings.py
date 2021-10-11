3bb653cc296a815a0e39abe2533635e2232e9298
GJSayers <gemma.sayers1@btinternet.com>
2021-10-11 10:17:00 +0000

    Set up settings files static css & media links

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -146,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
