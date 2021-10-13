059c3a0f31a308972161d4488700c8dc9ce30ba8
GJSayers <gemma.sayers1@btinternet.com>
2021-10-13 21:25:41 +0000

    Populate properties data to collections.html properties div

--- products/views.py
+++ products/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
-from .models import Product, ProductCategory, ProductType
+from .models import (Product, ProductCategory,
+                    ProductType, Ring) # JewelleryProperties
 
 
 def collections(request):
     """
     A view to show all products / product collections
     """
-
+    
     products = Product.objects.all()
     categories = ProductCategory()
     product_type = ProductType()
+    # rings = Ring.objects.all()
+    # ring_sizes = JewelleryProperties.RingSizes
 
     context = {
         'products': products,
         'categories': categories,
         'product_type': product_type,
+        # 'rings': rings,
+        # 'ring_sizes': ring_sizes,
     }
     return render(request, 'products/collections.html', context)
