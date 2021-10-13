604d00c2c66c71075baa286d34739778600cfd2d
GJSayers <gemma.sayers1@btinternet.com>
2021-10-13 14:08:40 +0000

    Add customisation for Admin backend

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
 from .models import Product, ProductCategory, ProductType, Ring, Clothing
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
-admin.site.register(ProductType)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_sku',
+        'product_name',
+        'product_category',
+        'product_type',
+        'product_price',
+        'rrp_price',
+        'image',
+    )
+    ordering = ('product_sku',)
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'product_type',
+    )
+
+    ordering = ('friendly_name',)
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'product_categories',
+    )
+
+    ordering = ('friendly_name',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Ring)
 admin.site.register(Clothing)
