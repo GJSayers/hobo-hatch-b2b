f8406369be2c750988b76ceacc5185275a6232a0
GJSayers <gemma.sayers1@btinternet.com>
2021-10-19 15:23:39 +0000

    Add secret key in settings key default in settings.py

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mk@mq35taqp-vkev!v@ylsm5b5w&p^26y8fdg(vu0j!3kj@&y&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
