cc9b39263e94de30ae2982be6ebc617162f67bb3
GJSayers <gemma.sayers1@btinternet.com>
2021-10-19 14:57:42 +0000

    Deployment phase 3 - Reinstate If statement for DB and allow heroku as a host in settings.py

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mk@mq35taqp-vkev!v@ylsm5b5w&p^26y8fdg(vu0j!3kj@&y&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hobo-hatch-b2b.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -104,12 +104,18 @@ WSGI_APPLICATION = 'hobo_hatch_b2b.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
