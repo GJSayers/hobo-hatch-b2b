9a6dde6565cdeb350bf86069d840a5f510aae597
GJSayers <gemma.sayers1@btinternet.com>
2021-10-20 17:44:22 +0000

    Deployment - AWS S3 bucket config and media files rules

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'multiselectfield',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -158,6 +159,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # AWS S3 bucket config
+    AWS_STORAGE_BUCKET_NAME = 'hobo-hatch-b2b'
+    AWS_S3_REGION_NAME = 'eu-central-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # For static and media files storage
+    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
