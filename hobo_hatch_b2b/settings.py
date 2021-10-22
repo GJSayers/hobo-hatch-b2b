0a88ad629d520e8f58bbd27a666457ddbd4824a0
GJSayers <gemma.sayers1@btinternet.com>
2021-10-22 14:43:26 +0000

    Uncomment Database URL statment in settings.py

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -108,17 +108,17 @@ WSGI_APPLICATION = 'hobo_hatch_b2b.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
