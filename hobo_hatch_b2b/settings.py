9e005e27381cbd09f4cabe40c75c430c62770678
GJSayers <gemma.sayers1@btinternet.com>
2021-10-22 08:53:44 +0000

    Rename custom_storage.py to plural and update references

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -143,9 +143,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
