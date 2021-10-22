a143ee189f14c81e529be3373d3440802aa95738
GJSayers <gemma.sayers1@btinternet.com>
2021-10-22 08:57:53 +0000

    Update product naming in template and settings to enable visibility on Heroku

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -178,9 +178,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # For static and media files storage
-    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
