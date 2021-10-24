f203f748c53005a34f5e8f168d2effb7b7b88012
GJSayers <gemma.sayers1@btinternet.com>
2021-10-24 18:30:40 +0000

    Add filter section to collections.html to enable filtering by category

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from model_utils.managers import InheritanceManager
+from django.contrib import messages
+from django.db.models import Q
 from .models import (Product, Category,
                      Type, Ring, Clothing)  # JewelleryProperties
 
@@ -10,16 +12,46 @@ def collections(request):
     """
     
     products = Product.objects.all()
+    categories = Category.objects.all()
+    product_type = Type.name
+    jewellery_size_type = Product.SizeType.jewellery_sizes
+    ring = Ring.objects.all()
+    clothing_sizes = Clothing(Product).ClothingSizes.choices
+    chosen_categories = None
+
+    if request.GET:
+        chosen_categories = request.GET.getlist('chosen_categories')
+        products = Product.objects.filter(product_category__in=Category.objects.filter(
+                                          name__in=chosen_categories))
+
+    context = {
+        'products': products,
+        'categories': categories,
+        'product_type': product_type,
+        'jewellery_size_type': jewellery_size_type,
+        'ring': ring,
+        'clothing_sizes': clothing_sizes,
+        'categories': categories,
+        'chosen_categories': chosen_categories,
+    }
+    return render(request, 'products/collections.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show full detail of selected product
+    """
+    
+    product = get_object_or_404(Product, pk=product_id)
     categories = Category.name
     product_type = Type.name
     jewellery_size_type = Product.SizeType.jewellery_sizes
     ring = Ring.objects.all()
     clothing_sizes = Clothing(Product).ClothingSizes.choices
-    print(jewellery_size_type)
-    print(ring)
+    
     
     context = {
-        'products': products,
+        'product': product,
         'categories': categories,
         'product_type': product_type,
         'jewellery_size_type': jewellery_size_type,
