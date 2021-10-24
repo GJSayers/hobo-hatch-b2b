35dcc5c8b9f102ce80d96303e0eb3c0751dc4237
GJSayers <gemma.sayers1@btinternet.com>
2021-10-24 19:40:07 +0000

    Create product_detail.html and assc. url

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.collections, name='collections')
+    path('', views.collections, name='collections'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
