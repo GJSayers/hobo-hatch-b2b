eba91bf6af7e6e4a5bd98785beed8ad8ad480798
GJSayers <gemma.sayers1@btinternet.com>
2021-10-25 12:44:37 +0000

    Create add to bag view and define in contexts.py & update urls

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
     delivery = Decimal(settings.STANDARD_DELIVERY_COST)
+    
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.product_price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+    
     grand_total = delivery + total
+    total_items = product_count
 
     context = {
         'bag_items': bag_items,
@@ -15,6 +31,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'grand_total': grand_total,
+        'total_items': total_items,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -6,3 +6,38 @@ def view_bag(request):
     A view to return the shopping cart / bag contents
     """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    Add product quantities to bag
+    """
+    
+    # quantity = int(request.POST.get('quantity'))
+    quantity_l = int(request.POST.get('quantity_l'))
+    quantity_n = int(request.POST.get('quantity_n'))
+    quantity_p = int(request.POST.get('quantity_p'))
+    quantity_s = int(request.POST.get('quantity_s'))
+    quantity_u = int(request.POST.get('quantity_u'))
+    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        # bag[item_id] += quantity
+        bag[item_id] += quantity_l
+        bag[item_id] += quantity_n
+        bag[item_id] += quantity_p
+        bag[item_id] += quantity_s
+        bag[item_id] += quantity_u
+    else:
+        # bag[item_id] = quantity
+        bag[item_id] = quantity_l
+        bag[item_id] = quantity_n
+        bag[item_id] = quantity_p
+        bag[item_id] = quantity_s
+        bag[item_id] = quantity_u
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
