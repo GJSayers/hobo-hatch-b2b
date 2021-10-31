a7211dc124df1a65b26ca7f1d313d56124250832
GJSayers <gemma.sayers1@btinternet.com>
2021-10-31 16:48:07 +0000

    Partial Bug Fix:  All product types adding to bag - Remaining Fix needed 1st Product not adding correctly

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,46 +7,30 @@ from products.models import Product, Category
 def bag_contents(request):
 
     bag_items = []
-    total = 0
-    total_items = 0
     bag = request.session.get('bag', {})
     delivery = Decimal(settings.STANDARD_DELIVERY_COST)
-    # ring = Product.product_type(3)
-    # clothing = Product.product_type(5,6,7,8,9,10)
-    # universal = Product.one_size
     
     for item_id, item_data in bag.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.product_price
-            total_items += item_data
-            bag_items.append({ 
-                'item_id': item_id,
-                'type': product.product_type,
-                'quantity_l': item_data,
-                'quantity_n': item_data,
-                'quantity_p': item_data,
-                'quantity_s': item_data,
-                'quantity_u': item_data,
-                'quantity': item_data,
-                'product': product,
-            })
-            print("bag items", bag.items())
-            print("bag_contents", item_data)
+       
+        product = get_object_or_404(Product, pk=item_id)
+        product_type = str(product.product_type)
+        size_qtys = item_data[product_type]
+        # total += item_data * product.product_price
+        # total_items += item_data
+        bag_items.append({ 
+            'item_id': item_id,
+            'product_type': str(product.product_type),
+            'size_qty': size_qtys,
+            'product': product,
+        })
+        print("bag items", bag_items)
 
-    grand_total = delivery + total
-    total_items = total_items
+    # grand_total = delivery + total
 
     context = {
-        #'item_id': item_id,
         'bag_items': bag_items,
-        'total': total,
-        'total_items': total_items,
-        'delivery': delivery,
-        'grand_total': grand_total,
-        # 'ring': ring,
-        # 'clothing': clothing,
-        # 'universal': universal,
+        # 'delivery': delivery,
+        # 'grand_total': grand_total,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from products.models import Product, Type
+# from django.utils.datastructures import MultiValueDict
 
 
 def view_bag(request):
@@ -13,69 +14,72 @@ def add_to_bag(request, item_id):
     """
     Add product quantities to bag
     """
-    ring = request.POST.get('rings')
+    rings = request.POST.get('rings')
     clothing = request.POST.get('knitwear')
-    if ring:
-        quantity_l = int(request.POST.get('quantity_l'))
-        quantity_n = int(request.POST.get('quantity_n'))
-        quantity_p = int(request.POST.get('quantity_p'))
-        quantity_s = int(request.POST.get('quantity_s'))
-        quantity_u = int(request.POST.get('quantity_u'))
-        redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    def add_quantities(size_qty, product_type, item_id, bag):
+        """
+        Identifies which product type is being added to the
+        bag to avoid overwriting.
+        breaking down the function into sections per size type
+        """
+        if 'rings' in request.POST:
+            product_type = request.POST.get('rings')
+        elif 'knitwear' in request.POST:
+            product_type = request.POST.get('knitwear')
         bag = request.session.get('bag', {})
-    
+        
+        if size_qty:
+            print(size_qty)
+            if item_id in list(bag.keys()):
+                if size_qty in bag[item_id][product_type].keys():
+                    bag[item_id][product_type] += size_qty
+                    print("+=", size_qty)
+                else:
+                    bag[item_id][product_type] = size_qty
+                    print("=", size_qty)
+            else:
+                bag[item_id] = {product_type: size_qty}
+                print("create", size_qty)
+
+    if rings:
+        if rings:
+            quantity_l = int(request.POST.get('quantity_l'))
+            quantity_n = int(request.POST.get('quantity_n'))
+            quantity_p = int(request.POST.get('quantity_p'))
+            quantity_s = int(request.POST.get('quantity_s'))
+            quantity_u = int(request.POST.get('quantity_u'))
+            product_type = request.POST.get('rings')
+            ring_size_qtys = {
+                                'quantity_l': quantity_l,
+                                'quantity_n': quantity_n,
+                                'quantity_p': quantity_p,
+                                'quantity_s': quantity_s,
+                                'quantity_u': quantity_u
+                            }
+            print("ring_size_qtys", ring_size_qtys)
+            add_quantities(ring_size_qtys, product_type, item_id, bag)
+        
     elif clothing:
-        quantity_xs = int(request.POST.get('quantity_xs'))
-        quantity_sm = int(request.POST.get('quantity_sm'))
-        quantity_m = int(request.POST.get('quantity_m'))
-        quantity_lg = int(request.POST.get('quantity_lg'))
-        quantity_xl = int(request.POST.get('quantity_xl'))
-        redirect_url = request.POST.get('redirect_url')
-        bag = request.session.get('bag', {})
-
-    else:
-        quantity = int(request.POST.get('quantity'))
-        redirect_url = request.POST.get('redirect_url')
-        bag = request.session.get('bag', {})
-
-    # if Product.clothing_size_matrix:
-
-    if item_id in list(bag.keys()):
-        print("bag keys", list(bag.keys()))
-        if ring:
-            bag[item_id] += quantity_l
-            bag[item_id] += quantity_n
-            bag[item_id] += quantity_p
-            bag[item_id] += quantity_s
-            bag[item_id] += quantity_u
-
-        elif clothing:
-            bag[item_id] += quantity_xs
-            bag[item_id] += quantity_sm
-            bag[item_id] += quantity_m
-            bag[item_id] += quantity_lg
-            bag[item_id] += quantity_xl
-
-        else:
-            bag[item_id] += quantity
-    else:
-        if ring:
-            bag[item_id] = quantity_l
-            bag[item_id] = quantity_n
-            bag[item_id] = quantity_p
-            bag[item_id] = quantity_s
-            bag[item_id] = quantity_u
-
-        elif clothing:
-            bag[item_id] = quantity_xs
-            bag[item_id] = quantity_sm
-            bag[item_id] = quantity_m
-            bag[item_id] = quantity_lg
-            bag[item_id] = quantity_xl
-
-        else:
-            bag[item_id] = quantity
+        if clothing:
+            quantity_xs = int(request.POST.get('quantity_xs'))
+            quantity_sm = int(request.POST.get('quantity_sm'))
+            quantity_m = int(request.POST.get('quantity_m'))
+            quantity_lg = int(request.POST.get('quantity_lg'))
+            quantity_xl = int(request.POST.get('quantity_xl'))
+            product_type = request.POST.get('knitwear')
+            clothing_size_qtys = {
+                                'quantity_xs': quantity_xs,
+                                'quantity_sm': quantity_sm,
+                                'quantity_m': quantity_m,
+                                'quantity_lg': quantity_lg,
+                                'quantity_xl': quantity_xl
+                                }
+            print("clothing_size_qtys", clothing_size_qtys)
+            add_quantities(clothing_size_qtys, product_type, item_id, bag)
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    print("session bag", request.session['bag'])
     return redirect(redirect_url)
