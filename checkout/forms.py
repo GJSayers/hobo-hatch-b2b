3c5c6c3389a373e9803855e4d520d6d82dae1050
GJSayers <gemma.sayers1@btinternet.com>
2021-11-08 19:01:24 +0000

    Add checkout app order foms & assc. signals to overide the standard forms

---
+++ checkout/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('buyer_name', 'stockist','buyer_phone',
+                  'buyer_email','accounts_phone',
+                  'address_1','address_2', 'town_or_city',
+                  'county_or_state', 'postcode',
+                  'country', 'delivery_date',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'buyer_name': 'Buyer Name',
+            'stockist': 'Store Name',
+            'buyer_email': 'Buyer Email Address',
+            'accounts_phone': 'Accounts Phone Number',
+            'address_1': 'Address Line 1',
+            'address_2': 'Address Line 2',
+            'town_or_city': 'Town or City',
+            'county_or_state': 'County or State',
+            'postcode': 'Postcode / Zipcode',
+            'country': 'Country',
+            'delivery_date': 'Preferred Date of Delivery',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
