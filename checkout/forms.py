8de30cfed7f89e2e1e23c8e0dd37ffca7e2dcc7a
GJSayers <gemma.sayers1@btinternet.com>
2021-11-10 11:32:51 +0000

    Add basic fomatting to allauth account forms & update country field on order form

--- checkout/forms.py
+++ checkout/forms.py
@@ -29,17 +29,17 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'county_or_state': 'County or State',
             'postcode': 'Postcode / Zipcode',
-            'country': 'Country',
         }
 
         # not possible to enter due to bug / 'delivery_date': 'Preferred Date of Delivery',
 
         self.fields['buyer_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
