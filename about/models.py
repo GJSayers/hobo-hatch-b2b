8659e33a0aef0f7d8c0aefdbf89507a4389ea38a
GJSayers <gemma.sayers1@btinternet.com>
2021-11-16 08:46:09 +0000

    Create models, migrate, register with admin and upload testimonials fixtures to about app

--- about/models.py
+++ about/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+
+class Testimonial(models.Model):
+    """
+    Class to define Testimonial for the about page
+    """
+    class Meta:
+        """
+        Class to designate plural name
+        """
+        verbose_name_plural = 'Testimonials'
+
+    stockist = models.ForeignKey(   
+        UserProfile, null=True, blank=False, on_delete=models.SET_NULL)
+    buyer_name = models.CharField(max_length=90, null=False, blank=False)
+    testimonial = models.TextField(max_length=300, null=False, blank=False)
--- about/views.py
+++ about/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, request,
+from django.shortcuts import render
 
 
 def about(request):
