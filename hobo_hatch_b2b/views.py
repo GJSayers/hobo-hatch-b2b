485de405f4397fc65cc1861295591a69a66a739c
GJSayers <gemma.sayers1@btinternet.com>
2021-11-26 09:59:00 +0000

    Add error pages

---
+++ hobo_hatch_b2b/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def error_404(request, exception):
+    """
+    Display 404 error message and home link
+    """
+    return render(request, 'error_handlers/error_404.html')
+
+
+def error_403(request, exception):
+    """
+    Display 403 error message and home link
+    """
+    return render(request, 'error_handlers/error_403.html')
+
+
+def error_400(request, exception):
+    """
+    Display 400 error message and home link
+    """
+    return render(request, 'error_handlers/error_400.html')
