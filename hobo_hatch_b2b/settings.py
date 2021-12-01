c5ff2d082df65ac765b9594fe0089f3cc155a3a0
GJSayers <gemma.sayers1@btinternet.com>
2021-12-01 09:56:31 +0000

    Switch off debugging

--- hobo_hatch_b2b/settings.py
+++ hobo_hatch_b2b/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hobo-hatch-b2b.herokuapp.com', 'localhost']
 
